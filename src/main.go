6267a41cdbb337f6094cae60fbf24041bd301ce7
Manuel Rodríguez Santana <lolo@turitop.com>
2022-11-16 10:58:10 +0000

    Add gitignore + sakila schema + sakila data + client logic

--- src/main.go
+++ src/main.go
@@ -1,32 +1,65 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"os"
-	"time"
 
-	"github.com/manolors/go-mariadb-sample/config"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
-func main() {
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags),
-		logger.Config{
-			SlowThreshold: time.Second,
-			LogLevel:      logger.Silent,
-			Colorful:      true,
-		},
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	DBName   string
+	Password string
+}
+
+func DbURL(dbConfig *DBConfig) string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.DBName,
 	)
+}
 
-	_, err := gorm.Open(mysql.Open(config.DbURL(config.BuildDBConfig())), &gorm.Config{
-		Logger: newLogger,
-	})
-
-	if err != nil {
-		log.Fatal("Error al conectar a la base de datos:", err)
+func main() {
+	godotenv.Load("../.env")
+	config := DBConfig{
+		User:     os.Getenv("DATABASE_USER"),
+		Password: os.Getenv("ROOT_PASSWORD"),
+		Host:     os.Getenv("DATABASE_HOST"),
+		Port:     os.Getenv("DATABASE_PORT"),
+		DBName:   os.Getenv("DATABASE_NAME"),
 	}
+	fmt.Printf("dbconfig: %#v\n", config)
+	db, err := sql.Open("mysql", DbURL(&config))
+	if err != nil {
+		log.Println("Error connecting database")
+	}
+	defer db.Close()
+
+	results, err := db.Query("SELECT first_name, last_name FROM actor")
+	if err != nil {
+		log.Println("Error retrieving data")
+	}
+	type Actor struct {
+		first_name string
+		last_name  string
+	}
+	for results.Next() {
+		var actor Actor
+		err = results.Scan(&actor.first_name, &actor.last_name)
+		if err != nil {
+			panic(err.Error())
+		}
+		log.Printf("Actor: %s %s\n", actor.first_name, actor.last_name)
+	}
+
 }
